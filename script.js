b5a8bd2fe03efa4be1137ae9f941152b427b1655
NeoGabe <gabrielg28@hotmail.com>
2020-10-29 23:51:30 -0500

    Create charactor and confirm variables

--- script.js
+++ script.js
@@ -14,10 +14,42 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numeric = [1,2,3,4,5,6,7,8,9,0];
+  var specialChar = ["!","#","$","%","&","*","<","=",">","?","@","[","]","{","}","~"]
+
+  var passwordLength = prompt("Please choose charactors between 8 and 128.");
+
+    if( passwordLength < 8 || passwordLength > 128) {
+      alert("Please choose numbers between 8 and 128");
+    }
+
+  var lowerCaseAns = confirm("Would you like LOWER CASE charactors?");
+  var upperCaseAns = confirm("Would you like UPPER CASE charactors?");
+  var numericAns = confirm("Would you like any NUMBERS?");
+  var specialCharAns = confirm("Would you like any SPECIAL CHARACTORS?");
+
+    if(lowerCase) {
+      for( var i = 0; i < lowerCase.length; ++i) {
+        
+      }
+    }
+
+    if(!lowerCaseAns && !upperCaseAns && !numericAns && !specialCharAns) {
+      alert("You must choose at least one charator type.");
+      return;
+    }
+
+
+
+
+
 
 
 // Here we ask the user if they eat steak, and store the result (true/false) in a variable.
 var eatSteak = confirm("Do you eat steak?");
+
 
 // If the user eats steak (eatSteak === true) we run the following code block.
 if (eatSteak) {
