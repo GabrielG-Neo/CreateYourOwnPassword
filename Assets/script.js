52cacd06973d10eda9bc2a784766823745e5f4fd
NeoGabe <gabrielg28@hotmail.com>
2020-11-15 10:44:44 -0600

    forgotten commits

--- Assets/script.js
+++ Assets/script.js
@@ -6,16 +6,13 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password
-  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-function generatePassword() {
   var passwordLetr = "";
-  var thePassword = "";
+  
   
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -68,10 +65,7 @@ function generatePassword() {
         console.log();
       }
     }
-    for (i = 0; i < numCharLength; i++) {
-      var index = Mathfloor(Math.random() + passwordLetr.length);
-      thePassword = thePassword + passwordLetr[index];
-      }
-    }
-   
+  
+  
+    
   
