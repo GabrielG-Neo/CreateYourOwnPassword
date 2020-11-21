8ba343c88f6f979b23beb613e9ae7e2177dade54
NeoGabe <gabrielg28@hotmail.com>
2020-11-21 17:04:20 -0600

    final bug fixes for functionality

--- Assets/script.js
+++ Assets/script.js
@@ -4,19 +4,15 @@ var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
 function writePassword() {
-  
-  
-}
+  }
 
-
-// Add event listener to generate button
+  // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword());
 
 function generatePassword() {
  
   var passwordLetr = [];
   var thePassword = [];
-  
   
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -67,19 +63,16 @@ function generatePassword() {
 
     if (specialCharAns) {
       for ( i = 0; i < specialChar.length; i++) {
-        passwordLetr.push(specialCharAns[i]);
+        passwordLetr.push(specialChar[i]);
         console.log(specialCharAns);
       }
     }
   
     for (i = 0; i < numCharLength; i++) {
-      var index = Math.floor(Math.random() * thePassword.length);
+      var index = Math.floor(Math.random() * passwordLetr.length);
       thePassword = thePassword + passwordLetr[index];
+      }
       passwordText.value = thePassword;
-      console.log(thePassword);
-    }
-    
-    passwordText.value = thePassword;
   }
   
   
