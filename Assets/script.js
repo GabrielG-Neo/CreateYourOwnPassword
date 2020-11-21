0ceee2a43f9223a5f20d98a2e1b7d2f2ccd57799
NeoGabe <gabrielg28@hotmail.com>
2020-11-21 16:35:12 -0600

    generating a password fixes

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
 function writePassword() {
-  var passwordText = document.querySelector("#password");
-      passwordText.value = password
-  generatePassword();
+  
+  
 }
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
 
 function generatePassword() {
-
+ 
   var passwordLetr = [];
   var thePassword = [];
+  
   
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -23,7 +24,8 @@ function generatePassword() {
   var specialChar = ["!","#","$","%","&","*","<","=",">","?","@","[","]","{","}","~"]
   var passwordLength = prompt("Please choose charactors between 8 and 128.");
   var numCharLength = parseInt(passwordLength);
-
+  
+ 
     if (passwordLength < 8 || passwordLength > 128) {
       alert("Please choose a number between 8 and 128");
       return
@@ -31,51 +33,53 @@ function generatePassword() {
 
     else {alert("Your password will be "+ numCharLength +" charactors long")}
 
-  var lowerCaseAns = confirm("Would you like LOWER CASE charactors?");
-  var upperCaseAns = confirm("Would you like UPPER CASE charactors?");
-  var numericAns = confirm("Would you like any NUMBERS?");
-  var specialCharAns = confirm("Would you like any SPECIAL CHARACTORS?");
+    var lowerCaseAns = confirm("Would you like LOWER CASE charactors?");
+    var upperCaseAns = confirm("Would you like UPPER CASE charactors?");
+    var numericAns = confirm("Would you like any NUMBERS?");
+    var specialCharAns = confirm("Would you like any SPECIAL CHARACTORS?");
+  
 
     if (!lowerCaseAns && !upperCaseAns && !numericAns && !specialCharAns) {
       alert("You must choose at least one charactor type.");
       return
     }
 
-    if (lowerCaseAns) {
+     if (lowerCaseAns) {
       for( var i = 0; i < lowerCase.length; i++) {
         passwordLetr.push(lowerCase[i]);
-        console.log();
+        console.log(lowerCaseAns);
       }
     }
 
     if (upperCaseAns) {
       for (var i = 0; i < upperCase.length; i++) {
         passwordLetr.push(upperCase[i]);
-        console.log();
+        console.log(upperCaseAns);
       }
     }
 
     if (numericAns) {
       for (var i = 0; i < numeric.length; i++) {
         passwordLetr.push(numeric[i]);
-        console.log();
+        console.log(numericAns);
       }
     }
 
     if (specialCharAns) {
       for ( i = 0; i < specialChar.length; i++) {
         passwordLetr.push(specialCharAns[i]);
+        console.log(specialCharAns);
       }
     }
   
-    for (i = 0; i < numCharLength.Length; i++) {
-      var index = Math.floor(Math.random() * passwordLetr.length);
-
-      thePassword = thePassword+passwordLetr[index];
+    for (i = 0; i < numCharLength; i++) {
+      var index = Math.floor(Math.random() * thePassword.length);
+      thePassword = thePassword + passwordLetr[index];
+      passwordText.value = thePassword;
+      console.log(thePassword);
     }
-
-   
-
+    
+    passwordText.value = thePassword;
   }
   
   
